8954e92e72fc841e842393dbde9d8b501ab27b55
afei <88603982@qq.com>
2016-05-29 17:07:35 +0800

    initial

---
+++ src/FetchJson.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: feizheng
+ * Date: 5/29/16
+ * Time: 5:06 PM
+ */
+class FetchJson
+{
+  public static function post()
+  {
+  }
+
+  public static function get()
+  {
+  }
+}
