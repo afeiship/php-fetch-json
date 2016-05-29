84e53d383cb0785a406d4c0a94f4bf757cdf651e
afei <88603982@qq.com>
2016-05-29 18:41:42 +0800

    update post/get

--- src/FetchJson.php
+++ src/FetchJson.php
@@ -8,11 +8,43 @@
  */
 class FetchJson
 {
-  public static function post()
+  public static function post($inUrl, $inData, $inHeaders = [])
   {
+    $headers = array_merge([
+      'Content-Type: application/json; charset=utf-8',
+    ], $inHeaders);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_URL, $inUrl);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $inData);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return [
+      'code' => $httpCode,
+      'data' => json_decode($response)
+    ];
   }
 
-  public static function get()
+
+  public static function get($inUrl, $inData, $inHeaders = [])
   {
+    $query_string = http_build_query($inData);
+    $http_url = implode('?', [$inUrl, $query_string]);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $http_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $inHeaders);
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    return [
+      'code' => $httpCode,
+      'data' => json_decode($response)
+    ];
   }
 }
---
+++ test/test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: feizheng
+ * Date: 5/29/16
+ * Time: 5:08 PM
+ */
+include '../src/FetchJson.php';
+
+$query_data = [
+  'redirect_url' => urlencode('http://www.baidu.com'),
+  'scope' => 'wechat'
+];
+
+//echo $query_data['scope'];
+$url = 'http://ci.com/api/test/account';
+$data = [
+  'id' => 1
+];
+$rs = FetchJson::get($url, $data);
+print_r($rs);
